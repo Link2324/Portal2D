bd58444ebbd41f6c9e43d9c54d4e7ae24c805aa9
Mangal <mangalk2324@gmail.com>
2021-09-05 14:25:46 +0530

    added in .gitignore

--- res/670930.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="670930" tilewidth="64" tileheight="64" tilecount="656" columns="41">
- <image source="D:/Wallpaper/670930.jpg" width="2659" height="1080"/>
-</tileset>
--- res/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="64" tileheight="64" tilecount="4" columns="2">
- <image source="tiles.png" width="128" height="128"/>
-</tileset>
